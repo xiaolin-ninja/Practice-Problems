bea963ffa1ccd85cf90789f27b25c1183582f107
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-04-03 21:34:32 -0500

    vending machine optimized

--- object_orientation.py
+++ object_orientation.py
@@ -8,10 +8,13 @@ class VendingMachine():
 
     def vend(self, selection, item_money):
         for item in self.items:
+            # check if this item is the selection
             if item['code'].lower() == selection.lower():
+                # check first if in stock
                 if item['quantity'] < 1:
                     return "{}: Out of stock!".format(item['name'])
 
+                # cachier bot
                 elif item_money == item['price']:
                     item['quantity'] -= 1
                     self.money += item_money
@@ -24,6 +27,7 @@ class VendingMachine():
                     return "Vending {} with {:.2f} change.".format(item['name'],
                        change)
 
+                # this ain't no charity machine
                 elif item_money < item['price']:
                     return "Not enough money!"
 
