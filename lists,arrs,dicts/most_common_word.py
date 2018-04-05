a3d9545eec74c1383c8d2e540d1209c512e16a54
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-04-05 00:32:19 -0700

    most common word in phrase & most commn phrase in text

--- lists,arrs,dicts/most_common_word.py
+++ lists,arrs,dicts/most_common_word.py
@@ -5,37 +5,40 @@ def common_word(phrase):
     """
 
     words = phrase.split()
-    words_count = {}
-    largest_count = 0
-    common_words = []
+    # words_count = {}
+    # common_words = []
 
-    for word in words:
-        words_count[word] = words_count.get(word, 0) + 1
-        if words_count[word] >= largest_count:
-            largest_count = words_count[word]
-            common_words.append(word)
+    # create dictionary
+    words_count = { word: words.count(word) for word in words }
+    # for word in words:
+    #     words_count[word] = words_count.get(word, 0) + 1
+
+    common_words = [ word for word, count in words_count.items()
+                     if count == max(words_count.values()) ]
+
+    # for word, count in words_count.items():
+    #     if count == max(words_count.values()):
+    #         common_words.append(word)
 
     return common_words
 
-print common_word('I really like a friend of a friend')
 
+def phrase_count(text, phrase):
+    """find most common phrase given a block of text
+    >>> phrase_count("The fox jumps over the moon moon the moon moon",
+    ['the','moon', 'moon'])
+    2
+    """
 
-# def phrase_count(text, phrase):
-#     """find most common phrase given a block of text
-#     >>> phrase_count("The fox jumps over the moon moon the moon moon",
-#     ['the','moon', 'moon'])
-#     2
-#     """
+    text = text.split()
+    count = 0
 
-#     text = text.split()
-#     count = 0
+    for i, word in enumerate(text):
+        if word == phrase[0]:
+            if text[i:i+len(phrase)] == phrase:
+                count += 1
 
-#     for i, word in enumerate(text):
-#         if word == phrase[0]:
-#             if text[i:i+len(phrase)] == phrase:
-#                 count += 1
-
-#     return count
+    return count
 
 # print phrase_count("The fox jumps over the moon moon the moon moon",
 #     ['the','moon', 'moon'])
