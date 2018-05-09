0ee36ad3483e2b23f704c0e41ad313445d193490
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-05-09 20:14:40 +0200

    conditional for one bottle of beer

--- Ruby/beer_song.rb
+++ Ruby/beer_song.rb
@@ -1,12 +1,17 @@
 def beer_bottles()
 	puts 'How many beers are on the wall?'
 	beers = gets.chomp
-	while beers > 0
-		puts beers + ' bottles of beers on the wall'
-		puts beers + ' bottles of beers!'
+	while beers.to_i > 0
+		if beers.to_i == 1
+			puts "\n" + beers + ' bottle of beers on the wall, ' + 
+			 beers + ' bottle of beers!'
+		else
+			puts "\n" + beers + ' bottles of beers on the wall, ' + 
+				 beers + ' bottles of beers!'
+		end
 		puts 'You take one down, pass it around,'
 		beers = (beers.to_i - 1).to_s
-		puts beers + 'bottles of beers on the wall!'
+		puts beers + " bottles of beers on the wall! \n"
 	end
 end
 
