372d6a92a9ed2c0c25f2a54c97056007a6a2c241
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-05-09 20:33:00 +0200

    random num between a and b

--- Ruby/deaf_grandma.rb
+++ Ruby/deaf_grandma.rb
@@ -1,18 +1,22 @@
 def deaf_grandma()
 	puts "Hello my little pumpkin' nugget! Welcome to grandma's house!"
 	count = 0
-	while True
+	while count
 		puts '>'
 		chat = gets.chomp
-		if chat.upcase != chat.downcase
+		if chat.upcase == chat
 			puts "HUH?! SPEAK UP, SONNY!"
-		if chat.upcase == 'BYE'
-			count += 1
-			if count == 3
-				break
+			if chat.upcase == 'BYE'
+				count += 1
+					if count == 3
+						break
+					end
+			end
 		else
 			count = 0
-			puts 'NO, NOT SINCE' + rand(1930, 1950)
+			puts 'NO, NOT SINCE' + rand(1930..1950)
 		end
+	end
+end
 
 deaf_grandma()
