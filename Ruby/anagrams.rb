4cece26467ea78d39bbcad945d467b1a54b273d5
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-05-13 18:40:29 +0200

    anagrams

--- Ruby/anagrams.rb
+++ Ruby/anagrams.rb
@@ -3,9 +3,7 @@ end
 
 def first_anagram?(a, b)
 	# run time:
-	# split string = O(n)
-	# permute = O(n!)
-	# check if b in a = O(n)
+	# O(n * n!) runtime
 	perms = a.chars.permutation.to_a
 	if perms.include?(b):
 		return true
@@ -14,6 +12,7 @@ end
 
 
 def second_anagram?(a, b)
+	# O(n^2) runtime
 	a, b = a.chars, b.chars
 
 	a.each do |letter|
@@ -23,4 +22,18 @@ def second_anagram?(a, b)
 	end
 
 	b.empty?
+end
+
+def third_anagram?(a, b)
+	# O(n) run time
+	return a.chars.sort == b.chars.sort
+end
+
+def fourth_anagram(a, b)
+	# O(n) runtime
+	a, b = a.chars, b.chars
+	letters = a.each_with_object(Hash.new(0)) { |letter, letters| letters[letter] += 1}
+	b.each { |letter| letters[letter] -= 1}
+
+	return letters.values.max == 0
 end
