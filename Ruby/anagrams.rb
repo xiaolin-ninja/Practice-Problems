53843651fd33de32d2cfb75a20d73dcce308803a
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-05-13 19:01:12 +0200

    add bennchmarks

--- Ruby/anagrams.rb
+++ Ruby/anagrams.rb
@@ -1,15 +1,18 @@
-def anagram?(a, b)
-end
+require 'benchmark'
+
 
 def first_anagram?(a, b)
 	# run time:
 	# O(n * n!) runtime
 	perms = a.chars.permutation.to_a
-	if perms.include?(b):
-		return true
+	perms.map! { |perm| perm.join("") }
+	return true if perms.include?(b)
 	return false
 end
 
+p first_anagram?('racecar', 'crrceaa')
+
+puts Benchmark.measure {first_anagram?('racecar', 'crrcea')}
 
 def second_anagram?(a, b)
 	# O(n^2) runtime
@@ -24,12 +27,20 @@ def second_anagram?(a, b)
 	b.empty?
 end
 
+p second_anagram?('racecar', 'crrceaa')
+
+puts Benchmark.measure {second_anagram?('racecar', 'crrcea')}
+
 def third_anagram?(a, b)
 	# O(n) run time
 	return a.chars.sort == b.chars.sort
 end
 
-def fourth_anagram(a, b)
+p third_anagram?('racecar', 'crrceaa')
+
+puts Benchmark.measure {third_anagram?('racecar', 'crrcea')}
+
+def fourth_anagram?(a, b)
 	# O(n) runtime
 	a, b = a.chars, b.chars
 	letters = a.each_with_object(Hash.new(0)) { |letter, letters| letters[letter] += 1}
@@ -37,3 +48,7 @@ def fourth_anagram(a, b)
 
 	return letters.values.max == 0
 end
+
+p fourth_anagram?('racecar', 'crrceaa')
+
+puts Benchmark.measure {fourth_anagram?('racecar', 'crrcea')}
