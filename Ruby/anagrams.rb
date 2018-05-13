aa4070333698af22263f989d0f1e939650e4f5cb
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-05-13 18:22:31 +0200

    second anagram

--- Ruby/anagrams.rb
+++ Ruby/anagrams.rb
@@ -1,12 +1,26 @@
 def anagram?(a, b)
 end
 
-def first_anagram?(a)
+def first_anagram?(a, b)
 	# run time:
 	# split string = O(n)
 	# permute = O(n!)
-	a.chars.permutation.to_a
+	# check if b in a = O(n)
+	perms = a.chars.permutation.to_a
+	if perms.include?(b):
+		return true
+	return false
 end
 
 
 def second_anagram?(a, b)
+	a, b = a.chars, b.chars
+
+	a.each do |letter|
+		idx = b.find_index(letter)
+		return false unless idx
+		b.slice!(idx)
+	end
+
+	b.empty?
+end
