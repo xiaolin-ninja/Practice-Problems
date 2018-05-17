c02b665bb6e3e5568dd342ecf6d8b0cf40243873
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-05-17 12:11:18 +0200

    reformat beers

--- Ruby/beer_song.rb
+++ Ruby/beer_song.rb
@@ -1,17 +1,17 @@
 def beer_bottles()
 	puts 'How many beers are on the wall?'
-	beers = gets.chomp
-	while beers.to_i > 0
-		if beers.to_i == 1
-			puts "\n" + beers + ' bottle of beers on the wall, ' + 
-			 beers + ' bottle of beers!'
+	beers = gets.chomp.to_i
+	while beers > 0
+		if beers == 1
+			puts "\n #{beers} bottle of beers on the wall, 
+				 #{beers} bottle of beers!"
 		else
-			puts "\n" + beers + ' bottles of beers on the wall, ' + 
-				 beers + ' bottles of beers!'
+			puts "\n #{beers} bottles of beers on the wall, 
+				 #{beers} bottles of beers!"
 		end
 		puts 'You take one down, pass it around,'
-		beers = (beers.to_i - 1).to_s
-		puts beers + " bottles of beers on the wall! \n"
+		beers -= 1
+		puts "#{beers} bottles of beers on the wall! \n"
 	end
 end
 
